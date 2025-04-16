e6bd92d6b712106b75fe5bf7368e6ec3f3a92b08
Ori Gold <ori.gold@humansecurity.com>
2025-04-15 22:38:17 -0700

    removing unnecessary clone

--- service_bindings/customer-service/src/index.ts
+++ service_bindings/customer-service/src/index.ts
@@ -7,7 +7,7 @@ interface Env {
 export default {
   async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     // Forward the request to HUMAN Service
-    const { contextId, request, response } = await env.HUMAN_ENFORCER_SERVICE.enforce(req.clone());
+    const { contextId, request, response } = await env.HUMAN_ENFORCER_SERVICE.enforce(req);
 
     if (response) {
       return response;
@@ -17,6 +17,6 @@ export default {
     const originResponse = await fetch(request);
 
     // Call postEnforce with the response from the origin
-    return await env.HUMAN_ENFORCER_SERVICE.postEnforce(contextId, originResponse.clone());
+    return await env.HUMAN_ENFORCER_SERVICE.postEnforce(contextId, originResponse);
   },
 }; 
